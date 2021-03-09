b4cd5a6a8d38e8024a599df95ab47b8719e025e9
Thomas Lane <parkerlane@Thomass-Mac-mini.local>
2021-03-09 09:24:02 -0500

    Fixed Bug

--- pages/campgrounds/[campid].js
+++ pages/campgrounds/[campid].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import Head from 'next/head';
-import SecondNavigation from '../../components/subpageNav';
+import SecondNavigation from '../../components/secondNavigation';
 import {
   getAllCampgrounds,
   getSingleCampground,
