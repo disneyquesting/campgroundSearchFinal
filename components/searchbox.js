316db409f2b35c93165339a7318b6aa78a4502b0
Thomas Lane <parkerlane@Thomass-Mac-mini.local>
2021-04-19 11:12:12 -0400

    Updates

--- components/searchbox.js
+++ components/searchbox.js
@@ -1,10 +1,10 @@
-import Link from 'next/link';
-import { Form, Formik, Field, FieldProps } from 'formik';
-import Router, { useRouter } from 'next/router';
-import { useQuery, useLazyQuery, gql } from '@apollo/client';
-import MultiSelect from 'react-multi-select-component';
-import { useState } from 'react';
-import SelectField from './CustomSelect';
+import Link from "next/link";
+import { Form, Formik, Field, FieldProps } from "formik";
+import Router, { useRouter } from "next/router";
+import { useQuery, useLazyQuery, gql } from "@apollo/client";
+import MultiSelect from "react-multi-select-component";
+import { useState } from "react";
+import SelectField from "./CustomSelect";
 
 export default function SearchBox({
   regions,
@@ -119,10 +119,10 @@ export default function SearchBox({
   let uniqueData = {};
 
   const initialValues = {
-    region: query.region || 'all',
-    camptype: query.camptype || 'all',
-    city: query.city || 'all',
-    campfeatures: query.campfeatures || 'all',
+    region: query.region || "all",
+    camptype: query.camptype || "all",
+    city: query.city || "all",
+    campfeatures: query.campfeatures || "all",
   };
 
   const {
@@ -135,20 +135,20 @@ export default function SearchBox({
       variables: {
         string: query.region,
       },
-      onCompleted: info => {
+      onCompleted: (info) => {
         setCityList(initialCityList);
         info.regions.nodes.length >= 1
-          ? info.regions.nodes[0].campgrounds.edges.map(cities => {
-              setCityList(prevState => [
+          ? info.regions.nodes[0].campgrounds.edges.map((cities) => {
+              setCityList((prevState) => [
                 ...prevState,
                 cities.node.acfDetails.city,
               ]);
-              console.log('City in Region: ', cities.node.acfDetails.city);
+              console.log("City in Region: ", cities.node.acfDetails.city);
             })
           : setCityList(initialCityList);
       },
     },
-    (uniqueData = [...new Set(citylist.map(town => town))])
+    (uniqueData = [...new Set(citylist.map((town) => town))])
   );
 
   const {
@@ -157,9 +157,9 @@ export default function SearchBox({
     data: cityData,
   } = useQuery(MAP_CITY_DATA, {
     variables: {
-      string: query.city === 'all' ? 'Twin Mountain' : query.city,
+      string: query.city === "all" ? "Twin Mountain" : query.city,
     },
-    onCompleted: info => {
+    onCompleted: (info) => {
       const lat = info
         ? parseFloat(info.campgrounds.nodes[0].acfDetails.latitude.toFixed(4))
         : 44.43;
@@ -181,7 +181,7 @@ export default function SearchBox({
         zoom: info ? 11 : 2,
       });
 
-      console.log('City updated.');
+      console.log("City updated.");
     },
   });
 
@@ -189,28 +189,28 @@ export default function SearchBox({
   const { loading, error, data, refetch } = useQuery(GET_SEARCH_RESULTS, {
     variables: {
       features:
-        query.campfeatures != 'all' ? query.campfeatures?.split(',') : [''],
-      regions: query.region != 'all' ? query.region : [''],
-      ownerships: query.camptype != 'all' ? query.camptype : [''],
+        query.campfeatures != "all" ? query.campfeatures?.split(",") : [""],
+      regions: query.region != "all" ? query.region : [""],
+      ownerships: query.camptype != "all" ? query.camptype : [""],
     },
-    onCompleted: info => {
+    onCompleted: (info) => {
       info.campgrounds.nodes.length >= 1
-        ? info.campgrounds.nodes.map(campground => {
+        ? info.campgrounds.nodes.map((campground) => {
             console.log(campground.title);
           })
-        : console.log('No campgrounds found');
+        : console.log("No campgrounds found");
     },
   });
 
   if (error) return `Error! ${error} Please try again.`;
 
-  const handleSubmit = async values => {
+  const handleSubmit = async (values) => {
     const campfeatures = Array.isArray(values.campfeatures)
-      ? values.campfeatures.map(({ value }) => value).join(',')
-      : (values.campfeatures = 'all');
+      ? values.campfeatures.map(({ value }) => value).join(",")
+      : (values.campfeatures = "all");
     Router.push(
       {
-        pathname: '/camps',
+        pathname: "/camps",
         query: { ...values, campfeatures },
       },
       undefined,
@@ -236,7 +236,7 @@ export default function SearchBox({
               className="input"
             >
               <option value="all">All</option>
-              {regions.nodes.map(region => {
+              {regions.nodes.map((region) => {
                 return (
                   <option key={region.id} value={region.name}>
                     {region.name}
@@ -258,7 +258,7 @@ export default function SearchBox({
               className="input"
             >
               <option value="all">All</option>
-              {camptypes.nodes.map(camp => {
+              {camptypes.nodes.map((camp) => {
                 return (
                   <option key={camp.id} value={camp.name}>
                     {camp.name}
@@ -282,7 +282,7 @@ export default function SearchBox({
             />
           </div>
 
-          {query.region !== 'all' ? (
+          {query.region !== "all" ? (
             <div className="field">
               <label id="search-cities" className="label" htmlFor="city">
                 City in Region
@@ -295,8 +295,8 @@ export default function SearchBox({
                 className="input"
               >
                 <option value="all">All</option>
-
-                {uniqueData.map(town => {
+                {uniqueData.sort()}
+                {uniqueData.map((town) => {
                   return (
                     <option key={town} value={town}>
                       {town}
